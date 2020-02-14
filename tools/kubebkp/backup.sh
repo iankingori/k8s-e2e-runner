915757eeac38330d091eeb589d6103f9e8be702d
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2020-02-14 16:46:24 +0000

    Encrypt and upload archive

--- tools/kubebkp/backup.sh
+++ tools/kubebkp/backup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-BACKUP_DIR=$(mktemp -d)
-ARCHIVE_NAME="backup-$(date +%Y-%m-%d_%H-%M).tar.gz"
-
 log_msg() {
 	echo "$(date -R) - $1"
 }
 
 log_msg "Starting backup..."
 
+# Create backup folders
+BACKUP_DIR=$(mktemp -d)
 mkdir -p "${BACKUP_DIR}/configmaps"
 mkdir -p "${BACKUP_DIR}/secrets"
 
+# Backup configmaps
 if [ -z "${BACKUP_CONFIGMAPS}" ]; then
 	log_msg "BACKUP_CONFIGMAPS is empty. Not backing up any configmaps."
 else
@@ -21,6 +21,7 @@ else
 	done
 fi
 
+# Backup secrets
 if [ -z "${BACKUP_SECRETS}" ]; then
 	log_msg "BACKUP_SECRETS is empty. Not backing up any secrets."
 else
@@ -30,9 +31,37 @@ else
 	done
 fi
 
-log_msg "Creating archive '${ARCHIVE_NAME}'..."
-tar -zcf "/tmp/${ARCHIVE_NAME}" "${BACKUP_DIR}"
+# Create backup archive
+ARCHIVE_NAME="backup-$(date +%Y-%m-%d_%H-%M)"
+log_msg "Creating archive '${ARCHIVE_NAME}.tar.gz'..."
+tar -Pzcf "/tmp/${ARCHIVE_NAME}.tar.gz" "${BACKUP_DIR}"
 rm -rf "${BACKUP_DIR}"
 
-# TODO: Upload archive "/tmp/${ARCHIVE_NAME}"
+# Encrypt backup archive
+if [ -z "${ENCRYPTION_KEY}" ]; then
+	log_msg "ENCRYPTION_KEY is empty. Skipping archive encryption and upload..."
+	exit
+else
+	log_msg "Encrypting archive /tmp/${ARCHIVE_NAME}.tar.gz..."
+	# Create key
+	openssl rand -out "/tmp/${ARCHIVE_NAME}.key" 32
+	# Encrypt archive
+	openssl enc -in "/tmp/${ARCHIVE_NAME}.tar.gz" -out "/tmp/${ARCHIVE_NAME}.tar.gz.enc" -pass file:"/tmp/${ARCHIVE_NAME}.key"
 
+	# Encrypt key using public ssh key
+	openssl rsautl -encrypt -pubin -inkey <(ssh-keygen -e -f "${ENCRYPTION_KEY}" -m PKCS8) \
+		-in "/tmp/${ARCHIVE_NAME}.key" -out "/tmp/${ARCHIVE_NAME}.key.enc"
+
+	rm "/tmp/${ARCHIVE_NAME}.tar.gz"
+	rm "/tmp/${ARCHIVE_NAME}.key"
+fi
+
+# Upload backup archive
+for file in "${ARCHIVE_NAME}.tar.gz.enc" "${ARCHIVE_NAME}.key.enc"; do
+	log_msg "Uploading file ${file}..."
+	az storage blob upload --container-name "${AZURE_STORAGE_CONTAINER_PROW_BKP}" \
+		--account-key "${AZURE_STORAGE_ACCOUNT_KEY}" --file "/tmp/${file}" --name "${file}"
+done
+
+rm "/tmp/${ARCHIVE_NAME}.key.enc"
+rm "/tmp/${ARCHIVE_NAME}.tar.gz.enc"
