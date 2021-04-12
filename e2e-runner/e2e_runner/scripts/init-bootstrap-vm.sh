43add7ba6df8389c0d198ec8fa9c1d5781a1c6b5
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-04-12 18:39:35 +0300

    Update CI Azure images
    * Bump stable K8s version to `v1.21.0`.
    * Bump Golang version to `1.16.3`.
    * Bump CI Azure images version to `2021.04.13`.
    * Small cleanup for the Packer Windows PowerShell scripts.
    * Use explicit `elevated_execute_command` for the Packer PowerShell
      provisioner steps.
    * Use Packer `manifest` post-processor.

--- e2e-runner/e2e_runner/scripts/init-bootstrap-vm.sh
+++ e2e-runner/e2e_runner/scripts/init-bootstrap-vm.sh
@@ -3,7 +3,7 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-GO_VERSION="1.16"
+GO_VERSION="1.16.3"
 KIND_BIN_URL="https://github.com/kubernetes-sigs/kind/releases/download/v0.10.0/kind-linux-amd64"
 
 function retrycmd_if_failure() {
