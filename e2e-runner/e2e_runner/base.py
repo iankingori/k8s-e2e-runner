42a58f218e64a757dd1c2cdda7a1fce6d579beba
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-04-13 09:11:39 +0300

    Decrease timeouts
    * Use `6h` for Prowjobs' executions and Azure RGs age.
    * Use `2h` for `kubetest` tests timeout.

--- e2e-runner/e2e_runner/base.py
+++ e2e-runner/e2e_runner/base.py
@@ -116,7 +116,7 @@ class CI(object):
         cmd.append("--dump=%s" % self.opts.artifacts_directory)
         cmd.append(
             ('--test_args=--ginkgo.flakeAttempts=1 '
-             '--test.timeout=12h '
+             '--test.timeout=2h '
              '--num-nodes=2 --ginkgo.noColor '
              '--ginkgo.dryRun=%(dryRun)s '
              '--node-os-distro=windows '
