4df615b19f2855f1ab7f759a01eaf48483c69637
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-06-18 17:29:17 +0300

    Refactor code executed as preKubeadmCommands
    Merge code executed as `preKubeadmCommands` into a single script
    called `kubeadm-bootstrap`.
    
    Upon successful execution, the script will write a file on the disk,
    which is validated by the K8s E2E runner.

--- e2e-runner/e2e_runner/scripts/kubeadm-bootstrap.ps1
+++ e2e-runner/e2e_runner/scripts/kubeadm-bootstrap.ps1
@@ -1,32 +1,252 @@
 Param(
-    [parameter(Mandatory=$true)]
-    [string]$CIPackagesBaseURL
+    [Parameter(Mandatory=$true)]
+    [String]$CIPackagesBaseURL,
+    [Parameter(Mandatory=$true)]
+    [String]$CIVersion,
+    [String]$SSHPublicKey,
+    [Switch]$K8sBins,
+    [Switch]$SDNCNIBins,
+    [Switch]$ContainerdBins,
+    [Switch]$ContainerdShimBins
 )
 
 $ErrorActionPreference = "Stop"
 
-curl.exe -s -o /tmp/utils.ps1 $CIPackagesBaseURL/scripts/utils.ps1
-. /tmp/utils.ps1
+$global:KUBERNETES_DIR = Join-Path $env:SystemDrive "k"
+$global:OPT_DIR = Join-Path $env:SystemDrive "opt"
+$global:CONTAINERD_DIR = Join-Path $env:SystemDrive "containerd"
+$global:RESULT_FILE = Join-Path $env:SystemDrive "tmp\kubeadm-success.txt"
 
 
-Set-Service -Name "wuauserv" -StartupType Disabled
-Stop-Service -Name "wuauserv"
-Set-MpPreference -DisableRealtimeMonitoring $true
-Set-PowerProfile -PowerProfile "Performance"
-Get-NetAdapter -Physical | Rename-NetAdapter -NewName "eth0"
-
-switch(Get-ContainerRuntime) {
-    "docker" {
-        Set-Service -Name "docker" -StartupType Automatic
-        Start-Service -Name "docker"
+function Start-ExternalCommand {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [Alias("Command")]
+        [ScriptBlock]$ScriptBlock,
+        [array]$ArgumentList=@(),
+        [string]$ErrorMessage
+    )
+    if($LASTEXITCODE){
+        # Leftover exit code. Some other process failed, and this
+        # function was called before it was resolved.
+        # There is no way to determine if the ScriptBlock contains
+        # a powershell commandlet or a native application. So we clear out
+        # the LASTEXITCODE variable before we execute. By this time, the value of
+        # the variable is not to be trusted for error detection anyway.
+        $LASTEXITCODE = ""
     }
-    "containerd" {
-        Add-Content -Path "/tmp/kubeadm-join-config.yaml" -Encoding Ascii `
-                    -Value "  criSocket: ${env:CONTAINER_RUNTIME_ENDPOINT}"
-        Start-ExternalCommand { nssm set containerd Start SERVICE_AUTO_START 2>$null }
-        Start-ExternalCommand { nssm start containerd 2>$null }
-        Wait-ReadyContainerd
+    $oldErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = "Continue"
+    $res = Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList
+    $ErrorActionPreference = $oldErrorActionPreference
+    if ($LASTEXITCODE) {
+        if(!$ErrorMessage){
+            Throw ("Command exited with status: {0}" -f $LASTEXITCODE)
+        }
+        throw ("{0} (Exit code: $LASTEXITCODE)" -f $ErrorMessage)
+    }
+    return $res
+}
+
+function Start-ExecuteWithRetry {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ScriptBlock]$ScriptBlock,
+        [int]$MaxRetryCount=10,
+        [int]$RetryInterval=3,
+        [string]$RetryMessage,
+        [array]$ArgumentList=@()
+    )
+    $currentErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = "Continue"
+    $retryCount = 0
+    while ($true) {
+        Write-Output "Start-ExecuteWithRetry attempt $retryCount"
+        try {
+            $res = Invoke-Command -ScriptBlock $ScriptBlock `
+                                  -ArgumentList $ArgumentList
+            $ErrorActionPreference = $currentErrorActionPreference
+            Write-Output "Start-ExecuteWithRetry terminated"
+            return $res
+        } catch [System.Exception] {
+            $retryCount++
+            if ($retryCount -gt $MaxRetryCount) {
+                $ErrorActionPreference = $currentErrorActionPreference
+                Write-Output "Start-ExecuteWithRetry exception thrown"
+                throw
+            } else {
+                if($RetryMessage) {
+                    Write-Output "Start-ExecuteWithRetry RetryMessage: $RetryMessage"
+                } elseif($_) {
+                    Write-Output "Start-ExecuteWithRetry Retry: $_.ToString()"
+                }
+                Start-Sleep $RetryInterval
+            }
+        }
     }
 }
 
-Start-ExternalCommand { nssm set kubelet Start SERVICE_AUTO_START 2>$null }
+function Start-FileDownload {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$URL,
+        [Parameter(Mandatory=$true)]
+        [string]$Destination,
+        [Parameter(Mandatory=$false)]
+        [int]$RetryCount=10
+    )
+    if(Test-Path $Destination) {
+        Remove-Item -Force $Destination
+    }
+    Start-ExecuteWithRetry -ScriptBlock {
+        curl.exe -C - -L -s -o $Destination $URL
+        if($LASTEXITCODE) {
+            Throw "Failed to download $URL"
+        }
+    } -MaxRetryCount $RetryCount -RetryInterval 3 -RetryMessage "Failed to download $URL. Retrying"
+}
+
+function Get-ContainerRuntime {
+    $dockerdBin = Get-Command "dockerd" -ErrorAction SilentlyContinue
+    if($dockerdBin) {
+        return "docker"
+    }
+    $containerd = Get-Command "containerd" -ErrorAction SilentlyContinue
+    if($containerd) {
+        return "containerd"
+    }
+    Throw "Could not find any container runtime installed"
+}
+
+function Stop-ContainerRuntime {
+    switch (Get-ContainerRuntime) {
+        "docker" {
+            Stop-Service "docker"
+        }
+        "containerd" {
+            Start-ExternalCommand { nssm stop containerd 2>$null }
+        }
+    }
+}
+
+function Wait-ReadyContainerd {
+    Start-ExecuteWithRetry -ScriptBlock {
+        $crictlInfo = Start-ExternalCommand { crictl info 2>$null }
+        if($LASTEXITCODE) {
+            Throw "Failed to execute: crictl info"
+        }
+        $crictlInfo = $crictlInfo | ConvertFrom-Json
+        $runtimeReady = $crictlInfo.status.conditions | Where-Object type -eq RuntimeReady
+        if(!$runtimeReady.status) {
+            Throw "The containerd runtime is not ready yet"
+        }
+        $networkReady = $crictlInfo.status.conditions | Where-Object type -eq NetworkReady
+        if(!$networkReady.status) {
+            Throw "The containerd network is not ready yet"
+        }
+    } -MaxRetryCount 30 -RetryInterval 10 -RetryMessage "Containerd is not ready yet"
+}
+
+function Set-PowerProfile {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("PowerSave", "Balanced", "Performance")]
+        [string]$PowerProfile
+    )
+    $guids = @{
+        "PowerSave" = "a1841308-3541-4fab-bc81-f71556f20b4a";
+        "Balanced" = "381b4222-f694-41f0-9685-ff5bb260df2e";
+        "Performance" = "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c";
+    }
+    Start-ExternalCommand { PowerCfg.exe /S $guids[$PowerProfile] }
+}
+
+function Install-OpenSSHServer {
+    # Install OpenSSH
+    Start-ExecuteWithRetry { Get-WindowsCapability -Online -Name OpenSSH* | Add-WindowsCapability -Online }
+    Set-Service -Name sshd -StartupType Automatic
+    Start-Service sshd
+
+    # Authorize SSH key (if given)
+    if($SSHPublicKey) {
+        $authorizedKeysFile = Join-Path $env:ProgramData "ssh\administrators_authorized_keys"
+        Set-Content -Path $authorizedKeysFile -Value $SSHPublicKey -Encoding ascii
+        $acl = Get-Acl $authorizedKeysFile
+        $acl.SetAccessRuleProtection($true, $false)
+        $administratorsRule = New-Object system.security.accesscontrol.filesystemaccessrule("Administrators", "FullControl", "Allow")
+        $systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYSTEM", "FullControl", "Allow")
+        $acl.SetAccessRule($administratorsRule)
+        $acl.SetAccessRule($systemRule)
+        $acl | Set-Acl
+    }
+
+    # Set PowerShell as default shell
+    New-ItemProperty -Force -Path "HKLM:\SOFTWARE\OpenSSH" -PropertyType String `
+                    -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+}
+
+function Update-Kubernetes {
+    $binaries = @("kubelet.exe", "kubeadm.exe")
+    foreach($bin in $binaries) {
+        Start-FileDownload "$CIPackagesBaseURL/$CIVersion/bin/windows/amd64/$bin" "$KUBERNETES_DIR\$bin"
+    }
+}
+
+function Update-SDNCNI {
+    $binaries = @("nat.exe", "sdnbridge.exe", "sdnoverlay.exe")
+    foreach($bin in $binaries) {
+        Start-FileDownload "$CIPackagesBaseURL/cni/$bin" "$OPT_DIR\cni\bin\$bin"
+    }
+}
+
+function Update-Containerd {
+    $binaries = @("containerd-stress.exe", "containerd.exe", "ctr.exe", "crictl.exe")
+    foreach($bin in $binaries) {
+        Start-FileDownload "$CIPackagesBaseURL/containerd/bin/$bin" "$CONTAINERD_DIR\$bin"
+    }
+}
+
+function Update-ContainerdShim {
+    Start-FileDownload "$CIPackagesBaseURL/containerd/bin/containerd-shim-runhcs-v1.exe" "$CONTAINERD_DIR\containerd-shim-runhcs-v1.exe"
+}
+
+
+try {
+    Install-OpenSSHServer
+    if($K8sBins) {
+        Update-Kubernetes
+    }
+    if($SDNCNIBins) {
+        Update-SDNCNI
+    }
+    if($ContainerdBins) {
+        Update-Containerd
+    }
+    if($ContainerdShimBins) {
+        Update-ContainerdShim
+    }
+    Set-Service -Name "wuauserv" -StartupType Disabled
+    Stop-Service -Name "wuauserv"
+    Set-MpPreference -DisableRealtimeMonitoring $true
+    Set-PowerProfile -PowerProfile "Performance"
+    Get-NetAdapter -Physical | Rename-NetAdapter -NewName "eth0"
+    switch(Get-ContainerRuntime) {
+        "docker" {
+            Set-Service -Name "docker" -StartupType Automatic
+            Start-Service -Name "docker"
+        }
+        "containerd" {
+            Add-Content -Path "/tmp/kubeadm-join-config.yaml" -Encoding Ascii `
+                        -Value "  criSocket: ${env:CONTAINER_RUNTIME_ENDPOINT}"
+            Start-ExternalCommand { nssm set containerd Start SERVICE_AUTO_START 2>$null }
+            Start-ExternalCommand { nssm start containerd 2>$null }
+            Wait-ReadyContainerd
+        }
+    }
+    Start-ExternalCommand { nssm set kubelet Start SERVICE_AUTO_START 2>$null }
+    $success = $true
+} catch [System.Exception] {
+    $success = $false
+} finally {
+    Set-Content -Path $RESULT_FILE -Value $success -Encoding ascii
+}
---
+++ e2e-runner/e2e_runner/scripts/kubeadm-bootstrap.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o pipefail
+set -o errexit
+
+if [[ $# -ne 4 ]]; then
+    echo "USAGE: $0 <CI_PACKAGES_BASE_URL> <CI_VERSION> <K8S_BINS_BUILT> <FLANNEL_MODE>"
+    exit 1
+fi
+
+CI_PACKAGES_BASE_URL=$1
+CI_VERSION=$2
+K8S_BINS_BUILT=$3
+FLANNEL_MODE=$4
+RESULT_FILE="/tmp/kubeadm-success.txt"
+
+run_cmd_with_retry() {
+    local RETRIES=$1
+    local WAIT_SLEEP=$2
+    local TIMEOUT=$3
+
+    shift && shift && shift
+
+    for i in $(seq 1 $RETRIES); do
+        timeout $TIMEOUT ${@} && break || \
+        if [ $i -eq $RETRIES ]; then
+            echo "Error: Failed to execute \"$@\" after $i attempts"
+            return 1
+        else
+            echo "Failed to execute \"$@\". Retrying in $WAIT_SLEEP seconds..."
+            sleep $WAIT_SLEEP
+        fi
+    done
+    echo Executed \"$@\" $i times;
+}
+
+update_k8s() {
+    CI_PACKAGES=("kubectl" "kubelet" "kubeadm")
+    CI_IMAGES=("kube-apiserver" "kube-controller-manager" "kube-proxy" "kube-scheduler")
+
+    echo "Updating Kubernetes to version: $CI_VERSION"
+
+    systemctl stop kubelet
+
+    for CI_PACKAGE in "${CI_PACKAGES[@]}"; do
+        PACKAGE_URL="$CI_PACKAGES_BASE_URL/$CI_VERSION/bin/linux/amd64/$CI_PACKAGE"
+        echo "* downloading binary: $PACKAGE_URL"
+        run_cmd_with_retry 10 3 10m curl --fail -Lo /usr/bin/$CI_PACKAGE $PACKAGE_URL
+        chmod +x /usr/bin/$CI_PACKAGE
+    done
+
+    systemctl start kubelet
+
+    CI_DIR="/tmp/k8s-ci"
+    mkdir -p $CI_DIR
+    for CI_IMAGE in "${CI_IMAGES[@]}"; do
+        CI_IMAGE_URL="$CI_PACKAGES_BASE_URL/$CI_VERSION/images/$CI_IMAGE.tar"
+        echo "* downloading package: $CI_IMAGE_URL"
+        run_cmd_with_retry 10 3 10m curl --fail -Lo "$CI_DIR/${CI_IMAGE}.tar" $CI_IMAGE_URL
+        ctr -n k8s.io images import "$CI_DIR/$CI_IMAGE.tar"
+        ctr -n k8s.io images tag "k8s.gcr.io/${CI_IMAGE}-amd64:${CI_VERSION//+/_}" "k8s.gcr.io/${CI_IMAGE}:${CI_VERSION//+/_}"
+        # remove unused image tag
+        ctr -n k8s.io image remove "k8s.gcr.io/${CI_IMAGE}-amd64:${CI_VERSION//+/_}"
+        # cleanup cached node image
+        crictl rmi "k8s.gcr.io/${CI_IMAGE}:v1.21.1"
+    done
+
+    echo "Checking binary versions"
+    echo "ctr version: $(ctr version)"
+    echo "kubeadm version: $(kubeadm version -o=short)"
+    echo "kubectl version: $(kubectl version --client=true --short=true)"
+    echo "kubelet version: $(kubelet --version)"
+}
+
+set_nics_mtu() {
+    for dev in $(find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\n'); do
+        /sbin/ifconfig "${dev}" mtu 1450
+    done
+}
+
+if [[ "$K8S_BINS_BUILT" = "True" ]]; then
+    update_k8s
+fi
+if [[ "$FLANNEL_MODE" = "overlay" ]]; then
+    set_nics_mtu
+fi
+echo "True" > $RESULT_FILE
