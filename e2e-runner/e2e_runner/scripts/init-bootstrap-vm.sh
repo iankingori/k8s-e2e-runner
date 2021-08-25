dabfb3e91d6f9b6d07131c989b195072acd383c2
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-08-25 11:12:14 +0300

    Bump golang to 1.17

--- e2e-runner/e2e_runner/scripts/init-bootstrap-vm.sh
+++ e2e-runner/e2e_runner/scripts/init-bootstrap-vm.sh
@@ -3,7 +3,7 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-GO_VERSION="1.16.7"
+GO_VERSION="1.17"
 KIND_BIN_URL="https://github.com/kubernetes-sigs/kind/releases/download/v0.11.1/kind-linux-amd64"
 
 function retrycmd_if_failure() {
