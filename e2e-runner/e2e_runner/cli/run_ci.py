58c887610e2fcc6dfa01700b37bf5770adc81a02
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2022-05-31 15:39:12 +0300

    Update --k8s-branch default value

--- e2e-runner/e2e_runner/cli/run_ci.py
+++ e2e-runner/e2e_runner/cli/run_ci.py
@@ -59,7 +59,7 @@ class RunCI(Command):
             default="https://github.com/kubernetes/kubernetes")
         p.add_argument(
             "--k8s-branch",
-            default=e2e_constants.DEFAULT_KUBERNETES_VERSION)
+            default="master")
 
         p.add_argument(
             "--containerd-repo",
