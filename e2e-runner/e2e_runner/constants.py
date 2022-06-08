1acb057eb547057543c11f2362a278fcf293e2ff
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2022-06-08 16:30:53 +0300

    Bump default K8s version to v1.24.1

--- e2e-runner/e2e_runner/constants.py
+++ e2e-runner/e2e_runner/constants.py
@@ -1,6 +1,6 @@
 AZURE_LOCATIONS = ["eastus2", "westeurope", "westus2", "southcentralus"]
 
-DEFAULT_KUBERNETES_VERSION = "v1.23.6"
+DEFAULT_KUBERNETES_VERSION = "v1.24.1"
 
 FLANNEL_MODE_OVERLAY = "overlay"
 FLANNEL_MODE_L2BRIDGE = "host-gw"
