1f6fe87e53adc76b6840182b70447a68a93bbba5
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2023-08-11 16:30:49 +0300

    Fix golang install script
    The response given by the calling `https://golang.org/VERSION`
    changed slightly.
    
    Only the first line contains the actual version we are interested in.
    
    Signed-off-by: Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>

--- e2e-runner/e2e_runner/ci/capz_flannel/cloud-init/install-golang.sh
+++ e2e-runner/e2e_runner/ci/capz_flannel/cloud-init/install-golang.sh
@@ -3,7 +3,7 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-GO_LATEST_VERSION=$(curl -s -L https://golang.org/VERSION\?m\=text)
+GO_LATEST_VERSION=$(curl -s -L https://golang.org/VERSION\?m\=text | head -1)
 
 curl -s -O https://dl.google.com/go/${GO_LATEST_VERSION}.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf ${GO_LATEST_VERSION}.linux-amd64.tar.gz
