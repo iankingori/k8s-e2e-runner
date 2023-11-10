54bd76843f0c63cdcc3eb2adb81320422807b0e3
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2023-11-10 12:19:17 +0200

    Use azure cloud provider helm repo from stable branch
    Signed-off-by: Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>

--- e2e-runner/e2e_runner/constants.py
+++ e2e-runner/e2e_runner/constants.py
@@ -31,5 +31,5 @@ DEFAULT_AKS_VERSION = "1.27"
 FLANNEL_MODE_OVERLAY = "overlay"
 FLANNEL_MODE_L2BRIDGE = "host-gw"
 
-CLOUD_PROVIDER_AZURE_HELM_REPO = "https://raw.githubusercontent.com/kubernetes-sigs/cloud-provider-azure/master/helm/repo"  # noqa: E501
+CLOUD_PROVIDER_AZURE_HELM_REPO = "https://raw.githubusercontent.com/kubernetes-sigs/cloud-provider-azure/{}/helm/repo"  # noqa: E501
 CLOUD_PROVIDER_AZURE_TAGS = "https://api.github.com/repos/kubernetes-sigs/cloud-provider-azure/tags"  # noqa: E501
