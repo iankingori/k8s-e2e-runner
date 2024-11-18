8f3a20567576634677321796a3fb88d553e544d4
Ian King'ori <50120072+iankingori@users.noreply.github.com>
2024-11-18 20:04:27 +0300

    fix remove go version restriction to 1.22 (#252)

--- e2e-runner/e2e_runner/ci/capz_flannel/cloud-init/install-golang.sh
+++ e2e-runner/e2e_runner/ci/capz_flannel/cloud-init/install-golang.sh
@@ -3,9 +3,7 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-GO_LATEST_VERSION=go1.21.7
-#TODO: switch back to latest go version after hcsshim takes: https://github.com/microsoft/go-winio/pull/312
-#$(curl -s -L https://golang.org/VERSION\?m\=text | head -1)
+GO_LATEST_VERSION=$(curl -s -L https://golang.org/VERSION\?m\=text | head -1)
 
 curl -s -O https://dl.google.com/go/${GO_LATEST_VERSION}.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf ${GO_LATEST_VERSION}.linux-amd64.tar.gz
